3b3664ba568d9d07e5b382827a2876607aa0a513
Andrew Schwedland <aschwedland17@gmail.com>
2024-08-26 10:48:41 -0600

    Adding final files for home revive website

--- construction/urls.py
+++ construction/urls.py
@@ -6,6 +6,12 @@ urlpatterns = [
     path('about', about, name='about'),
     path('services', services, name='services'),
     path('contact', contact, name='contact'),
-    path('specific-service', specific_service, name='specific-service'),
-    path('coming-soon', coming_soon, name='coming-soon')
+    path('whole-home-remodel', remodel, name='remodel'),
+    path('exterior-painting', exterior_painting, name='exterior-painting'),
+    path('interior-painting', interior_painting, name='interior-painting'),
+    path('fence-repair-installation', fence, name='fence'),
+    path('deck-repair-installation', deck, name='deck'),
+    path('appliance-repair', appliance_repair, name='appliance-repair'),
+    path('tile-installation', tile, name='tile-installation'),
+    path('wood-flooring-installation', wood, name='wood-flooring-installation'),
 ]
--- construction/views.py
+++ construction/views.py
@@ -5,8 +5,7 @@ def coming_soon(request):
     return render(request, 'coming-soon.html')
 
 def home(request):
-    return HttpResponseRedirect('coming-soon')
-    #return render(request, 'home.html')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
@@ -19,3 +18,27 @@ def contact(request):
 
 def specific_service(request):
     return render(request, 'specific-service.html')
+
+def remodel(request):
+    return render(request, 'remodel.html')
+
+def exterior_painting(request):
+    return render(request, 'exterior-painting.html')
+
+def interior_painting(request):
+    return render(request, 'interior-painting.html')
+
+def fence(request):
+    return render(request, 'fence.html')
+
+def deck(request):
+    return render(request, 'deck.html')
+
+def appliance_repair(request):
+    return render(request, 'appliance-repair.html')
+
+def tile(request):
+    return render(request, 'tile.html')
+
+def wood(request):
+    return render(request, 'wood.html')
